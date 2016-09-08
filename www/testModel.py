07ab917b593906ca711668e9e544c2d8c147790e
haiyangzhaoikina <zhy20090912@gmail.com>
2016-09-08 11:14:16 +0000

    add testModel

--- www/testModel.py
+++ www/testModel.py
@@ -6,10 +6,10 @@ from models import User,Blog,Comment
 @asyncio.coroutine
 def test( loop ):
     yield from orm.create_pool( loop = loop, user='root', password='root', db='awesome' )
-    u=User(name='test22',email='test22@test.com',admin = 1, passwd='test',image='about:blank')
+    u = User(name='Test', email='test@example.com', passwd='1234567890', image='about:blank')
     yield from u.save()
 
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
     loop.run_until_complete(test(loop))
-    loop.run_forever()
+    loop.close()
