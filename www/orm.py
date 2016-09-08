d561c26077eb72e396a01b0860cf0f92d9a7ad08
haiyangzhaoikina <zhy20090912@gmail.com>
2016-09-08 11:09:51 +0000

    add gitignore

--- www/orm.py
+++ www/orm.py
@@ -31,26 +31,28 @@ def create_pool(loop, **kw):
 def select(sql, args, size=None):
     log(sql, args)
     global __pool
-    with __pool.get() as conn:
-        with conn.cursor(aiomysql.DictCursor) as cur:
-            yield from cur.execute(sql.replace('?', '%s'), args or ())
-            if size:
-                rs = yield from cur.fetchmany(size)
-            else:
-                rs = yield from cur.fetchall()
+    with (yield from __pool) as conn:
+        cur = yield from conn.cursor(aiomysql.DictCursor)
+        yield from cur.execute(sql.replace('?', '%s'), args or ())
+        if size:
+            rs = yield from cur.fetchmany(size)
+        else:
+            rs = yield from cur.fetchall()
+        yield from cur.close()
         logging.info('rows returned: %s' % len(rs))
         return rs
 
 @asyncio.coroutine
 def execute(sql, args, autocommit=True):
     log(sql)
-    with __pool.get() as conn:
+    with (yield from __pool) as conn:
         if not autocommit:
             yield from conn.begin()
         try:
-            with conn.cursor(aiomysql.DictCursor) as cur:
-                yield from cur.execute(sql.replace('?', '%s'), args)
-                affected = cur.rowcount
+            cur = yield from conn.cursor()
+            yield from cur.execute(sql.replace('?', '%s'), args)
+            affected = cur.rowcount
+            yield from cur.close()
             if not autocommit:
                 yield from conn.commit()
         except BaseException as e:
